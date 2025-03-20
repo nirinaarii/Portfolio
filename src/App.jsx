b2df78fb501cf1fd1b1c9b69e116ddfaa69a7e87
nirinaarii <nirinaarii@gmail.com>
2025-03-20 10:07:41 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import "./App.css";
+import "./index.css";
+import { LoadingScreen } from './components/LoadingScreen';
+import { Navbar } from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
+import { Home } from './components/section/home';
+import { About } from './components/section/About';
+import { Projects } from './components/section/Projects';
+import {Contact} from './components/section/Contact';
+function App() {
+ const [isLoaded,setIsLoaded]= useState(false);
+ const [menuOpen,setMenuOpen]= useState(false);
+
+
+  return(
+    <>
+      {!isLoaded &&<LoadingScreen onComplete={()=> setIsLoaded(true)} />}
+      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+           <Home/>
+           <About/>
+           <Projects/>
+           <Contact/>
+      </div>
+    </>
+  );
+}
+
+export default App;
